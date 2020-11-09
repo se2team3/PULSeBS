c77e4ca383f4130b3b3096eb7defa06cd5f1491a
Federico Bitondo <s276294@studenti.polito.it>
2020-11-09 16:26:37 +0100

    PULSEBS-32 send email: setup the module and the config file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+require('dotenv').config({ path: './config/config.env' });
 const express = require('express');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app = new express();
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+
+const mail = require('./utils/mail');
+
+mail.send({
+    to: "s276294@asdasdasd.polito.it",
+    subject: "New email",
+    text: "Nuovo testo, leggi qua"
+})
+.then(console.log)
+.catch(console.error);
