d5fbb91997091de84647c06392ca517c9971b661
Federico Bitondo <s276294@studenti.polito.it>
2020-11-13 17:22:50 +0100

    restore db name

--- server/db/db.js
+++ server/db/db.js
@@ -3,14 +3,14 @@
 var sqlite = require('sqlite3').verbose();
 var path = require('path');
 
-const filename = 'PULSeBS.db';
+const filename = 'PULSeBS';
 const db_name = path.join(__dirname, "database", filename);
 
  const db = new sqlite.Database(db_name, (err) => {
     if(err) {
         console.error(err);
         throw err;
-    }
+    };
     console.log(`Connected to ${filename} database.`);
 }) 
 
