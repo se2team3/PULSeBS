245a140f282d666a456371e8ff9ff44805d81106
piazzadario <s276010@studenti.polito.it>
2020-11-15 17:05:19 +0100

    [PULSEBS-45] removed userService duplicated code

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,31 +1,5 @@
-const sleep = require('../utils/sleep');
 const userDao  = require('../daos/user_dao');
 const errHandler = require('./errorHandler');
-// harcoded users
-const users = [
-    { id: 1, username: "user1", password: "pass1", role: "Student" },
-    { id: 2, username: "user2", password: "pass2", role: "Student" },
-    { id: 3, username: "user3", password: "pass3", role: "Teacher" },
-    { id: 4, username: "user4", password: "pass4", role: "Booking Manager" },
-    { id: 5, username: "user5", password: "pass5", role: "Support Officer" },
-    { id: 6, username: "user6", password: "pass6", role: "Student" },
-];
-
-const login = async ({username, password}) => {
-    // TODO: is this necessary?
-    // sleep for a while
-    await sleep(1);
-    // check credentials
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        const { password, ...withoutPassword } = user;
-        return { ...withoutPassword };
-    }
-};
-
-// test purpose
-const addUser = (user) => users.push(user);
-const clearUsers = () => users.splice(0, users.length);
 
 const createUserTable = async function() {    
     try{
@@ -63,4 +37,4 @@ const deleteUsers = async function(){
     }
 }
 
-module.exports = { login, addUser, clearUsers, createUserTable, getUser, insertUser, deleteUsers};
+module.exports = { createUserTable, getUser, insertUser, deleteUsers};
