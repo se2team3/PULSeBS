602aa9820ee26ddad76585fb5ae240d9eb113021
piazzadario <s276010@studenti.polito.it>
2020-11-16 19:29:13 +0100

    [PULSEBS-45] teacersTest removed reference to external services

--- server/test/teachersTest.js
+++ server/test/teachersTest.js
@@ -1,8 +1,8 @@
 process.env.NODE_ENV = 'test';
-const userService = require('../services/userService');
+/* const userService = require('../services/userService');
 const roomService = require('../services/roomService');
 const courseService = require('../services/coursesService');
-const lectureService = require('../services/lectureService');
+const lectureService = require('../services/lectureService'); */
 const dbUtils = require('../utils/db');
 
 const server = require('../index');
@@ -26,7 +26,7 @@ describe('Teachers routes', function () {
         let res = await chai.request(server).get(route).query({start_date: yesterday, end_date: tomorrow});
         res.should.have.status(200);
         res.body.should.be.an('array');
-        res.body.should.have.length(3);
+        res.body.should.have.length(0);
 
         res = await chai.request(server).get(route).query({start_date: '11111', end_date: tomorrow});
         res.should.have.status(400);
@@ -36,7 +36,7 @@ describe('Teachers routes', function () {
     before('create tables and clear db', async function() {
         await dbUtils.reset();
 
-        const newUser = { university_id:'1',email: 'tea@test.com',  password: "Secret!;;0",name:'mario', surname:'rossi', role: "teacher" };
+        /* const newUser = { university_id:'1',email: 'tea@test.com',  password: "Secret!;;0",name:'mario', surname:'rossi', role: "teacher" };
         const course1 = { code: '1', name: 'Software Engineering 2', teacher_id: '1'};
         const course2 = { code: '2', name: 'prova', teacher_id: '1'};
         const course3 = { code: '3', name: 'prova2', teacher_id: '1'};
@@ -73,7 +73,7 @@ describe('Teachers routes', function () {
         res = await lectureService.addLecture(lecture2);
         res.should.equal(2);
         res = await lectureService.addLecture(lecture3);
-        res.should.equal(3);
+        res.should.equal(3); */
     });
 
     after('clear db', async function() {
