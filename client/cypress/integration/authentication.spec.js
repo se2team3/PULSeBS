bf4831483a13b82c5157bf1c7af28f4fedc2a7b3
Federico Bitondo <s276294@studenti.polito.it>
2020-11-16 13:31:36 +0100

    PULSEBS-36: dummy test for login form

---
+++ client/cypress/integration/authentication.spec.js
@@ -0,0 +1,47 @@
+describe('Header tests', () => {
+    before('visit the page', () => {
+        cy.visit('/');
+    });
+    before('clear the cookie', () => {
+        cy.clearCookie('token', { log: true });
+    });
+    it('should be unlogged', () => {
+        cy.contains('Login').should('exist');
+    });
+    it('should move to the login page', () => {
+        cy.contains('Login').click();
+        cy.url().should('match', '/login');
+    });
+    it('should display login modal', () => {
+        cy.get('#username').should('exist');
+        cy.get('#password').should('exist');
+        cy.get('.btn').contains('Login').should('exist');
+    });
+    it('should be focused on email field', () => {
+        cy.focused().should('have.id', 'username');
+    });
+    it('should be in invalid state when the email does not follow the pattern', () => {
+        cy.focused().type('mypartial');
+        cy.get('#password').focus();
+        cy.get('#username:invalid').should('exist');
+    });
+    it('should complete the invalid email', () => {
+        cy.get('#username').focus().type('@email.com');
+        cy.get('#password').focus();
+        cy.get('#username:invalid').should('not.exist');
+    });
+    it('should input a non visible password', () => {
+        const pass = 'my_password?!';
+        cy.get('#password').focus().type(pass);
+        cy.contains(pass).should('not.exist');
+    });
+    it('should properly login with valid credentials', () => {
+        cy.get('#username').focus().clear()
+            .type('valid@email.com');
+        cy.get('#password').focus().clear()
+            .type('valid_password')
+            .type('{enter}');
+        cy.getCookie('token').should('exist');
+        cy.url().should('match', '/');
+    });
+});
