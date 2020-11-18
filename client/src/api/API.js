9295b90966f5ffc5a866d06fa01fb94730674607
Luca Rinelli <rinelliluca@gmail.com>
2020-11-18 23:55:05 +0100

    login apis got deleted in merge...

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -5,6 +5,57 @@ import BookingExtended from './models/booking_extended';
 
 const baseURL = "/api";
 
+async function isAuthenticated() {
+    let url = "/user";
+    const response = await fetch(baseURL + url);
+    const userJson = await response.json();
+    if (response.ok) {
+        return userJson;
+    } else {
+        let err = { status: response.status, errObj: userJson };
+        throw err;  // An object with the error coming from the server
+    }
+}
+
+async function userLogin(username, password) {
+    return new Promise((resolve, reject) => {
+        fetch(baseURL + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: username, password: password }),
+        }).then((response) => {
+            if (response.ok) {
+                response.json().then((user) => {
+                    resolve(user);
+                });
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((obj) => { reject(obj); }) // error msg in the response body
+                    .catch((err) => { reject({messsage: "Cannot parse server response" })}); // something else
+            }
+        }).catch((err) => { reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
+    });
+}
+async function userLogout(username, password) {
+    return new Promise((resolve, reject) => {
+        fetch(baseURL + '/logout', {
+            method: 'POST',
+        }).then((response) => {
+            if (response.ok) {
+                resolve(null);
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((obj) => { reject(obj); }) // error msg in the response body
+                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
+            }
+        });
+    });
+}
+
 /**
  * Get all lectures, optionally filter by time frame or use specific role and user
  * This is used to call:
