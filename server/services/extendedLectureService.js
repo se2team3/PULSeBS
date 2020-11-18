98777a75ba2d8b360cc9be676c31f715d25bfb1a
Federico Bitondo <s276294@studenti.polito.it>
2020-11-18 15:39:18 +0100

    Fixed test URLs

--- server/services/extendedLectureService.js
+++ server/services/extendedLectureService.js
@@ -5,7 +5,6 @@ const errHandler = require('./errorHandler');
 exports.getLectureById = async function(lecture_id) {
     try {
         let lecture = await extendedLectureDao.getLectureById(lecture_id);
-        console.log(lecture)
         return lecture;
     } catch (error) {
         return errHandler(error);
