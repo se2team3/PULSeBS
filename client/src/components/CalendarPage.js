ba06b06260d0d44a8ab4424166cec594adafd395
Federico Bitondo <s276294@studenti.polito.it>
2020-11-23 12:55:06 +0100

    PULSeBS-55: update sidebar

--- client/src/components/CalendarPage.js
+++ client/src/components/CalendarPage.js
@@ -176,26 +176,27 @@ class CalendarPage extends React.Component {
         <AuthContext.Consumer>
           {(context) => (
             <Container fluid>
-              <Row >
+              <Row>
                 <Col sm={9} className="below-nav" >
                   {this.renderCalendar(this.props.authUser?.role)}
                 </Col>
 
                 <Col sm={3} className="sidebar">
-                  <Nav className="col-md-12 d-none d-md-block bg-light sidebar">
-                    <h2>Courses</h2>
+                  <Nav className="px-4 py-4 col-md-12 d-none d-md-block bg-light sidebar">
+                    <h2 className="mb-3">Courses</h2>
                     <Form>
-                      {this.state.events.map((e) => {
-                        if (showArray.indexOf(e.subjectId) === -1) {
-                          showArray.push(e.subjectId)
-                          return <CourseBadge
-                              key = {e.subjectId}
-                              {...e}
-                              handleClick = {this.changeDisplayEvent}
-                          />
-                        }
-                        else return null
-                      })}
+                      {
+                        this.state.events.map((e) => {
+                          if (showArray.indexOf(e.subjectId) === -1) {
+                            showArray.push(e.subjectId)
+                            return <CourseBadge
+                                key = {e.subjectId}
+                                {...e}
+                                handleClick = {this.changeDisplayEvent}
+                            />
+                          }
+                        })
+                      }
                     </Form>
                   </Nav>
                 </Col>
@@ -216,21 +217,33 @@ class CalendarPage extends React.Component {
 }
 
 function CourseBadge (props) {
+  const style = {
+    'backgroundColor': props.backgroundColor,
+  };
   return (
-    <h2 key={props.lectureId}>
-      <Badge style={{'backgroundColor': props.backgroundColor}}>
-        <Form.Check
+    <div className="rounded" style={style}>
+      <Row className="mb-3 w-100 d-flex justify-content-between align-items-center">
+        <Col lg={1} className="ml-3">
+          <Form.Check
             type="checkbox"
             defaultChecked="true"
             value={props.subjectId}
-            label={props.subjectName}
             onClick={(ev) => props.handleClick(props.subjectId, ev)}
-        />
-        <h4>
-          -Prof. {props.teacher}
-        </h4>
-      </Badge>
-    </h2>
+          />
+        </Col>
+        <Col className="align-items-center my-auto" lg={10}>
+          <span key={props.lectureId}>
+            <span className="font-weight-bold">
+              {props.subjectName}
+            </span>
+            <br/>
+            <span style={{'fontSize': '90%'}}>
+              Prof. {props.teacher}
+            </span>
+          </span>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
