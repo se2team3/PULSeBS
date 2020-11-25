20d88b5c59b8f3be49958db8d9f68be81aa07346
Federico Bitondo <s276294@studenti.polito.it>
2020-11-25 11:41:08 +0100

    PULSEBS-55: removing booking status from month view

--- client/src/components/CalendarPage.js
+++ client/src/components/CalendarPage.js
@@ -165,9 +165,12 @@ class CalendarPage extends React.Component {
             <div style={{'font-size': '110%', 'text-overflow': 'ellipsis', 'white-space': 'nowrap', 'overflow': 'hidden'}}>
               <b>{eventInfo.event.title}</b><br/>
               <i>{eventInfo.event._def.extendedProps.room}</i><br/>
-              <div style={{'color': 'rgb(255, 248, 220)', 'position': 'absolute', 'bottom': 0, 'left': '0.2em'}}>
-                <b>{eventInfo.event._def.extendedProps.stat}</b>
-              </div>
+              {
+                eventInfo.view.type !== "dayGridMonth" &&
+                <div style={{'color': 'rgb(255, 248, 220)', 'position': 'absolute', 'bottom': 0, 'left': '0.2em'}}>
+                  <b>{eventInfo.event._def.extendedProps.stat}</b>
+                </div>
+              }
             </div>
           )}
         }
