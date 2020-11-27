740d2fa3759555d2f11766751bef195ec9d21bd5
Federico Bitondo <s276294@studenti.polito.it>
2020-11-27 18:01:50 +0100

    PULSEBS-83: started testing with fixtures

--- client/cypress/integration/calendarStub.spec.js
+++ client/cypress/integration/calendarStub.spec.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress" />
 
 describe('Student calendar', () => {
+    /*
     before('Stub lecture\'s list', () => {
         cy.route2('/api/lectures', { fixture: 'list_of_lectures' });
     });
+    */
     before('Clear test db', () => {
         cy.db('clear');
     });
@@ -13,9 +15,22 @@ describe('Student calendar', () => {
     before('Visit calendar page', () => {
        cy.visit('/calendar');
     });
+    before('Aliases', () => {
+        cy.wrap('button[aria-label="prev"]').as('prev');
+        cy.wrap('button[aria-label="next"]').as('next');
+        cy.contains('today').as('today');
+        cy.contains('week').as('week');
+        cy.contains('list').as('list');
+        cy.contains('month').as('month');
+    })
     describe('Check calendar rendering', () => {
         it('should contain navigation button', () => {
-            cy.get ('button[aria-label="prev"]').should('exist');
+            cy.get ('@prev').should('exist');
+            cy.get ('@next').should('exist');
+            cy.get ('@today').should('exist');
+            cy.get ('@week').should('exist');
+            cy.get ('@list').should('exist');
+            cy.get ('@month').should('exist');
         });
     });
 });
