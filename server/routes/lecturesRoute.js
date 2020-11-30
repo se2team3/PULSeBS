51362730f6d7b33b4cc6af2e95dd55be8ddc62b4
LucaAntoniotti <luca.antoniotti97@gmail.com>
2020-11-30 15:36:05 +0100

    Merge branch 'master' of https://github.com/se2team3/PULSeBS

--- server/routes/lecturesRoute.js
+++ server/routes/lecturesRoute.js
@@ -9,18 +9,25 @@ const router = express.Router();
 
 /**
  * @swagger
- * /lectures:
+ * /lectures/:lecture_id/bookings:
  *  get:
  *    tags:
  *      - lectures
  *    summary: "Get the list of all bookings for a lesson"
  *    description: "Use to request all the bookings for a specific lesson"
+ *    parameters:
+ *      - in: path
+ *        name: lecture_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *          format: int64
  *    consumes:
  *       - "application/json"
  *    produces:
  *       - "application/json"
  *    responses:
- *       "201":
+ *       "200":
  *         description: "Successful response"
  *         schema:
  *           type: "array"
@@ -46,12 +53,19 @@ router.get('/lectures/:lecture_id/bookings', async(req,res) =>{
 
 /**
  * @swagger
- * /lectures:
+ * /lectures/:lecture_id:
  *  get:
  *    tags:
  *      - lectures
- *    summary: "Get the list of all bookings for a lesson"
- *    description: "Use to request all the bookings for a specific lesson"
+ *    summary: "Get all the information needed for a lesson"
+ *    description: "Use to request an extended lecture"
+ *    parameters:
+ *      - in: path
+ *        name: lecture_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *          format: int64
  *    consumes:
  *       - "application/json"
  *    produces:
@@ -80,110 +94,14 @@ router.get('/lectures/:lecture_id', async(req,res) =>{
   }
 })
 
-
-
-/**
+ /**
  * @swagger
- * /lectures:
- *  get:
+ * /lectures/{lecture_id}:
+ *  delete:
  *    tags:
  *      - lectures
- *    summary: "Get the list of all lectures"
- *    description: "Use to request all the lectures"
- *    consumes:
- *       - "application/json"
- *    produces:
- *       - "application/json"
- *    responses:
- *       "200":
- *         description: "Successful response"
- *         schema:
- *           type: "array"
- *           items:
- *             $ref: "#/components/schemas/Lecture"
- *       "400":
- *         description: "Invalid status value"
- *    security:
- *     - petstore_auth:
- *       - "write:pets"
- *       - "read:pets"
- */
-
-/*
-router.get('/lectures', (req, res) => {
-  //lectureDao.getAllLectures...
-  res.status(200).json([{ id: 1, course_id: "01SQNOV" }]);
-});
-
-
-/**
- * @swagger
- * /lectures:
- *  post:
- *      tags:
- *      - "lectures"
- *      summary: "Add a new lecture"
- *      description: ""
- *      operationId: ""
- *      consumes:
- *      - "application/json"
- *      - "application/xml"
- *      produces:
- *      - "application/xml"
- *      - "application/json"
- *      parameters:
- *      - in: "body"
- *        name: "body"
- *        description: "Lecture object that needs to be added to the db"
- *        required: true
- *        schema:
- *          $ref: "#/components/schemas/Lecture"
- *      responses:
- *        "405":
- *          description: "Invalid input"
- *        '201':
- *         description: Created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   format: int64
- *                   description: ID of the created lecture.
- *         # -----------------------------------------------------
- *         # Links
- *         # -----------------------------------------------------
- *         links:
- *           GetLectureById:   # <---- arbitrary name for the link
- *             operationId: getLecture
- *             # or
- *             # operationRef: '#/lectures/{lecture_id}/get'
- *             parameters:
- *               lecture_id: '$response.body#/id'
- *             description: >
- *               The `id` value returned in the response can be used as
- *               the `lecture_id` parameter in `GET /lectures/{lecture_id}`.
- *         # -----------------------------------------------------
- *    
- */
-
- /*
-router.post('/lectures', (req, res) => {
-  //lectureDao.getAllLectures...
-  res.status(200).json({ id: 1 });
-});
-
-
-/**
- * @swagger
- *  /lectures/{lecture_id}:
- *  get:
- *    tags:
- *      - "lectures"
- *    summary: Gets a lecture by ID
- *    operationId: getLecture
+ *    summary: "Delete a lecture"
+ *    description: "Use to request a deletion for a specific lecture"
  *    parameters:
  *      - in: path
  *        name: lecture_id
@@ -191,35 +109,10 @@ router.post('/lectures', (req, res) => {
  *        schema:
  *          type: integer
  *          format: int64
- *    responses:
- *      '200':
- *        description: A Lecture object
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Lecture'
- */
-
- /*
-
-router.get('/lectures/:lecture_id', (req, res) => {
-  //lectureDao.getAllLectures...
-  const lectureId = req.params.lecture_id;
-  res.status(200).json({ id: 1, course_id: "01SQNOV" });
-});*/
-
-
-
-
-
- /**
- * @swagger
- * /teachers/{teacher_id}/lectures/{lecture_id}:
- *  delete:
- *    tags:
- *      - lectures
- *    summary: "Delete a lecture"
- *    description: "Use to request a deletion for a specific lecture"
+ *      - in: cookie
+ *        name: user
+ *        schema:
+ *          type: object 
  *    consumes:
  *       - "application/json"
  *    produces:
