f12cd819acfc5f271988d0bbaad9851648942917
Luca Rinelli <rinelliluca@gmail.com>
2020-11-30 21:18:45 +0100

    PULSEBS-111 PULSEBS-113 LecturePage remote tests

--- client/cypress/integration/LecturePage.spec.js
+++ client/cypress/integration/LecturePage.spec.js
@@ -67,6 +67,61 @@ describe('Lecture page', () => {
             cy.contains('Cancel lecture').should('not.exist');
         })
     })
+
+    describe('change to remote lecture',()=>{
+        beforeEach('visit page', () => {
+            cy.intercept('GET','/api/user', { fixture: 'teacher1.json'})
+            cy.intercept('GET','/api/lectures/1/bookings', { fixture: 'lecture1bookings.json' })
+            
+        });
+
+        it('button visible more than 30 minutes before', () => {
+            cy.intercept('GET','/api/lectures/1', { fixture: 'lecture1.json' }).as('getLecture')
+            cy.clock(Date.UTC(2020,10,19,6,0),['Date'])
+            cy.visit('/lectures/1');
+            cy.wait('@getLecture');
+            cy.contains("Change to distance lecture");
+        })
+
+        it('has no yellow alert if not remote', () => {
+            cy.contains('REMOTE LECTURE').should('not.exist');
+        })
+
+        it('load lecture not remote', () => {
+            cy.intercept('GET','/api/lectures/1', {fixture:'lecture1.json'}).as('getLecture')
+            cy.clock(Date.UTC(2020,10,19,6,0),['Date'])
+            cy.visit('/lectures/1');
+            cy.wait(['@getLecture']);
+        })
+
+        it('button changes lecture to remote when clicked more than 30 minutes before', () => {
+            cy.intercept('PATCH','/api/lectures/1',{
+                statusCode: 200,
+                body: ''
+              }).as('patchLecture');
+            cy.intercept('GET','/api/lectures/1', {fixture:'lecture1remote.json'}).as('getLectureAfterRemote');//FIXME cypress does not like this for some reason
+            cy.contains("Change to distance lecture").click();
+            cy.wait('@patchLecture');
+            cy.wait('@getLectureAfterRemote');
+            cy.contains('Change to distance lecture').should('not.exist')
+        })
+
+        it('cancelled lecture has yellow alert', () => {
+            cy.contains('REMOTE LECTURE');
+        })
+
+        it('text for bookings changed', () => {
+            cy.contains("This lecture has been changed from 'presence' to `remote`. There were 3 bookings out of 35 available seats.");
+        })
+
+        it('button is not visible less than 30 minutes before', () => {
+            cy.intercept('GET','/api/lectures/1', { fixture: 'lecture1.json' }).as('getLecture')
+            cy.clock(Date.UTC(2020,10,19,7,30),['Date'])
+            cy.visit('/lectures/1');
+            cy.wait('@getLecture');
+            cy.contains('Change to distance lecture').should('not.exist');
+        })
+    })
     
     /*describe('presence lecture with bookings, real API',()=>{
         before('visit page', () => {
