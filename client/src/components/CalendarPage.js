99d5211eae14a03133f4449443e63b216e753703
Federico Bitondo <s276294@studenti.polito.it>
2020-12-02 23:10:53 +0100

    fix lecture status filters

--- client/src/components/CalendarPage.js
+++ client/src/components/CalendarPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Container, Col, Nav, Badge, Form } from 'react-bootstrap';
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, { eventTupleToStore } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list';
@@ -27,7 +27,9 @@ class CalendarPage extends React.Component {
       modal: false,
       selected: { extendedProps: { status: null } },
       lectures: [],
-      events: []
+      events: [],
+      courseFilters: [],
+      statusFilters: []
     }
   }
 
@@ -36,7 +38,6 @@ class CalendarPage extends React.Component {
       throw { status: 401, errorObj: "no authUser specified" }
     const lectures = await API.getLectures(this.state.startDate, this.state.endDate, this.props.authUser?.role, this.props.authUser?.id);
     this.setState({ lectures });
-    this.transformIntoEvents();
   }
 
   getStatus = (l) => {
@@ -50,7 +51,7 @@ class CalendarPage extends React.Component {
 
   getColor = (course_id) => {
     let colorArray = ["#31a831", "#ed425c","deepSkyBlue","darkOrange","#e37be3",
-    "peru","salmon","lightBlue", "lightSeaGreen"] 
+      "peru","salmon","lightBlue", "lightSeaGreen"]
     let ids = this.state.lectures.map((l) => l.course_id).filter(this.onlyUnique);
     let index = ids.indexOf(course_id);
 
@@ -67,55 +68,55 @@ class CalendarPage extends React.Component {
     return self.indexOf(value) === index;
   }
 
-  transformIntoEvents = () => {
-    this.setState(state => {
-      const list = state.lectures.map((l) => {
-        let diff = l.max_seats - l.booking_counter
-        let stat = this.getStatus(l)
-        return ({
-          lectureId: l.id,
-          subjectId: l.course_id,
-          subjectName: l.course_name,
-          teacher: l.teacher_name + " " + l.teacher_surname,
-          status: stat,
-          seats: diff,
-          title: l.course_name,
-          room: l.room_name,
-          start: l.datetime,
-          end: l.datetime_end,
-          backgroundColor: this.getColor(l.course_id),
-          display: 'auto',
-          textColor: 'black'
-        });
-      });
-      return { events: [...list] }
+  filterLectures = () => {
+    let filteredLectures = this.state.lectures.filter((lec) => {
+      let rightCourse = true;
+      if (this.state.courseFilters.length > 0) {
+        rightCourse = this.state.courseFilters.some((filter) => filter !== lec.course_id);
+      }
+
+      let rightStatus = true;
+      if (this.state.statusFilters.length > 0) {
+        rightStatus = this.state.statusFilters.some((filter) => filter === this.getStatus(lec));
+      }
+
+      return rightCourse && rightStatus;
     });
+
+    return (filteredLectures.length>0)? filteredLectures : [];
+  }
+
+  getCourses(lectures){
+    return [...new Set(lectures.map((lec) => lec.course_id))];
   }
 
   changeDisplayEvent = (filterName, filterValue, event) => {
-    let value;
-    event.target.checked === true ? value = 'auto' : value = "none"
     if (filterName === 'courseFilter') {
-      this.setState(state => {
-        const list = state.events.map((e) => {
-          if (e.subjectId === filterValue) {
-            e.display = value;
-          }
-          return e;
+      if(event.target.checked){
+        this.setState(state => {
+          const courseFilters = state.courseFilters.filter((cf)=>cf !== filterValue);
+          return {courseFilters: courseFilters}
         });
-        return { events: [...list] }
-      });
+      }else{
+        this.setState(state => {
+          const courseFilters = state.courseFilters.concat(filterValue);
+          return {courseFilters: courseFilters}
+        });
+      }
+
     } else if (filterName === 'statusFilter') {
-      value = (value === 'auto') ? 'none' : 'auto'; // This is due to the current design. Not a scalable solution for multiple status filters
-      this.setState(state => {
-        const list = state.events.map((e) => {
-          if (e.status !== filterValue) {
-            e.display = value;
-          }
-          return e;
+      if(event.target.checked){
+        this.setState(state => {
+          const statusFilters = state.statusFilters.concat(filterValue);
+          return {statusFilters: statusFilters}
         });
-        return { events: [...list] }
-      });
+      }else{
+        this.setState(state => {
+          const statusFilters = state.statusFilters.filter((sf)=>sf !== filterValue);
+          return {statusFilters: statusFilters}
+        });
+      }
+
     }
   }
 
@@ -148,21 +149,21 @@ class CalendarPage extends React.Component {
       setRole: (role) => this.role = role,
       manipulateDOM: (eventInfo) => {
         return (
-          <div style={{ 'fontSize': '110%', 'textOverflow': 'ellipsis', 'whiteSpace': 'nowrap', 'overflow': 'hidden' }}>
-            <b className="title">{eventInfo.event.title}</b><br/>
-            <i className="room">{eventInfo.event.extendedProps.room}</i><br/>
-            {
-              eventInfo.view.type === "timeGridWeek" &&
-              <div data-cy="booking_status" className="status" style={{ 'color': 'rgb(255, 248, 220)', 'position': 'absolute', 'bottom': 0, 'left': '0.2em' }}>
-                <b>{eventInfo.event._def.extendedProps.status.toUpperCase()}</b>
-              </div>
-            }
-          </div>
+            <div style={{ 'fontSize': '110%', 'textOverflow': 'ellipsis', 'whiteSpace': 'nowrap', 'overflow': 'hidden' }}>
+              <b className="title">{eventInfo.event.title}</b><br/>
+              <i className="room">{eventInfo.event.extendedProps.room}</i><br/>
+              {
+                eventInfo.view.type === "timeGridWeek" &&
+                <div data-cy="booking_status" className="status" style={{ 'color': 'rgb(255, 248, 220)', 'position': 'absolute', 'bottom': 0, 'left': '0.2em' }}>
+                  <b>{eventInfo.event._def.extendedProps.status.toUpperCase()}</b>
+                </div>
+              }
+            </div>
         )
       },
       onLectureClick: (info) => {
         if (this.role === 'student' && info.event.extendedProps.status !== LectureState.canceled
-        && info.event.extendedProps.status !== LectureState.remote) 
+            && info.event.extendedProps.status !== LectureState.remote)
         {this.setState({ modal: true, selected: info.event });}
         else if (this.role === 'teacher') this.props.goToLecturePage(info.event);
       },
@@ -181,29 +182,50 @@ class CalendarPage extends React.Component {
     }
   })()
 
+
+  transformIntoEvents = (l) => {
+    let diff = l.max_seats - l.booking_counter
+    let stat = this.getStatus(l)
+    return ({
+      lectureId: l.id,
+      subjectId: l.course_id,
+      subjectName: l.course_name,
+      teacher: l.teacher_name + " " + l.teacher_surname,
+      status: stat,
+      seats: diff,
+      title: l.course_name,
+      room: l.room_name,
+      start: l.datetime,
+      end: l.datetime_end,
+      backgroundColor: this.getColor(l.course_id),
+      display: 'auto',
+      textColor: 'black'
+    });
+  }
+
   renderCalendar = (role) => {
     this.eventHandler.setRole(role);
     return (
-      <FullCalendar
-        plugins={[timeGridPlugin, dayGridPlugin, listPlugin]}
-        initialView="timeGridWeek"
-        expandRows={true}
-        firstDay="1"
-        slotMinTime="08:00:00"
-        slotMaxTime="20:00:00"
-        nowIndicator={true}
-        allDaySlot={false}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "timeGridWeek,listWeek,dayGridMonth"
-        }}
-        events={this.state.events}
-        eventClick={this.eventHandler.onLectureClick}
-        eventContent={this.eventHandler.manipulateDOM}
-        datesSet={this.eventHandler.onViewChange}
-        eventClassNames={this.eventHandler.setClickable}
-      />
+        <FullCalendar
+            plugins={[timeGridPlugin, dayGridPlugin, listPlugin]}
+            initialView="timeGridWeek"
+            expandRows={true}
+            firstDay="1"
+            slotMinTime="08:00:00"
+            slotMaxTime="20:00:00"
+            nowIndicator={true}
+            allDaySlot={false}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "timeGridWeek,listWeek,dayGridMonth"
+            }}
+            events={this.filterLectures().map(this.transformIntoEvents)}
+            eventClick={this.eventHandler.onLectureClick}
+            eventContent={this.eventHandler.manipulateDOM}
+            datesSet={this.eventHandler.onViewChange}
+            eventClassNames={this.eventHandler.setClickable}
+        />
     );
 
   }
@@ -212,81 +234,82 @@ class CalendarPage extends React.Component {
   render() {
     let showArray = [];
     return (
-      <>
-        <AuthContext.Consumer>
-          {(context) => {
-            if (!context.authUser)
-              return null;
-            return (
-              <Container fluid>
-                <Row>
-                  <Col sm={9} className="below-nav calendar" >
-                    {this.renderCalendar(this.props.authUser?.role)}
-                  </Col>
+        <>
+          <AuthContext.Consumer>
+            {(context) => {
+              if (!context.authUser)
+                return null;
+              return (
+                  <Container fluid>
+                    <Row>
+                      <Col sm={9} className="below-nav calendar" >
+                        {this.renderCalendar(this.props.authUser?.role)}
+                      </Col>
 
-                  <Col sm={3} className="sidebar">
-                    {(this.props.authUser?.role === 'student') ?
-                    <Badge className='ml-2'>
-                      <Form.Check type="checkbox"
+                      <Col sm={3} className="sidebar">
+                        {(this.props.authUser?.role === 'student') ?
+                            <Badge className='ml-2'>
+                              <Form.Check type="checkbox"
 
-                        defaultChecked={false}
-                        label='Booked'
-                        style={{ fontSize: 20 }}
-                        onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.booked, ev)}
-                      />
-                    </Badge>
-                    :<Badge className='ml-2'>
-                    <Form.Check type="checkbox"
-
-                      defaultChecked={false}
-                      label='Cancelled'
-                      style={{ fontSize: 20 }}
-                      onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.canceled, ev)}
-                    />
-                  </Badge>}
-                    <br/>
-                    <Badge className='ml-2'>
-                      <Form.Check type="checkbox"
-                        defaultChecked={false}
-                        label='Remote'
-                        style={{ fontSize: 20 }}
-                        onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.remote, ev)}
-                      />
-                    </Badge>
-                    <Nav className="px-4 py-4 col-md-12 d-none d-md-block bg-light sidebar">
-                      <h2 className="mb-3">Courses</h2>
-
-                      <Form>
-                        {
-                          this.state.events.map((e) => {
-                            if (showArray.indexOf(e.subjectId) === -1) {
-                              showArray.push(e.subjectId)
-                              return <CourseBadge
-                                key={e.subjectId}
-                                {...e}
-                                handleClick={this.changeDisplayEvent}
+                                          defaultChecked={false}
+                                          label='Booked'
+                                          style={{ fontSize: 20 }}
+                                          onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.booked, ev)}
                               />
+                            </Badge>
+                            :<Badge className='ml-2'>
+                              <Form.Check type="checkbox"
+
+                                          defaultChecked={false}
+                                          label='Cancelled'
+                                          style={{ fontSize: 20 }}
+                                          onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.canceled, ev)}
+                              />
+                            </Badge>}
+                        <br/>
+                        <Badge className='ml-2'>
+                          <Form.Check type="checkbox"
+                                      defaultChecked={false}
+                                      label='Remote'
+                                      style={{ fontSize: 20 }}
+                                      onClick={(ev) => this.changeDisplayEvent('statusFilter', LectureState.remote, ev)}
+                          />
+                        </Badge>
+                        <Nav className="px-4 py-4 col-md-12 d-none d-md-block bg-light sidebar">
+                          <h2 className="mb-3">Courses</h2>
+
+                          <Form>
+                            {
+                              this.state.lectures.map(this.transformIntoEvents)
+                                  .map((e) => {
+                                    if (showArray.indexOf(e.subjectId) === -1) {
+                                      showArray.push(e.subjectId)
+                                      return <CourseBadge
+                                          key={e.subjectId}
+                                          {...e}
+                                          handleClick={this.changeDisplayEvent}
+                                      />
+                                    }
+                                    return null;
+                                  })
                             }
-                            return null;
-                          })
-                        }
-                      </Form>
-                    </Nav>
-                  </Col>
-                </Row>
-                <CalendarModal show={this.state.modal} closeModal={this.closeModal}
-                  bookLecture={() => this.bookLecture(context.authUser?.id ?? 1, this.state.selected.extendedProps.lectureId)}
-                  cancelBooking={() => this.cancelBooking(context.authUser?.id ?? 1, this.state.selected.extendedProps.lectureId)}
-                  lecture={this.state.selected} />
+                          </Form>
+                        </Nav>
+                      </Col>
+                    </Row>
+                    <CalendarModal show={this.state.modal} closeModal={this.closeModal}
+                                   bookLecture={() => this.bookLecture(context.authUser?.id ?? 1, this.state.selected.extendedProps.lectureId)}
+                                   cancelBooking={() => this.cancelBooking(context.authUser?.id ?? 1, this.state.selected.extendedProps.lectureId)}
+                                   lecture={this.state.selected} />
 
-              </Container>
+                  </Container>
 
-            )
-          }}
+              )
+            }}
 
 
-        </AuthContext.Consumer>
-      </>)
+          </AuthContext.Consumer>
+        </>)
   }
 }
 
