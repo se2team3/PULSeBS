cec93a8cd6451ea764c500a1f2efbb3b526e42e8
Federico Bitondo <s276294@studenti.polito.it>
2020-12-09 12:05:27 +0100

    changed authorize logic

--- server/services/authorizeService.js
+++ server/services/authorizeService.js
@@ -4,19 +4,19 @@ const { secret } = require('../config/secret.json');
 // TODO: we may want to use a set of roles (e.g. both Student and Teacher can access a resource)
 /**
  * Middleware function that filters request based on the given role
- * @param {string} [role] - specifies the role that has access to the resource.
+ * @param {string[]} [role] - specifies the roles that has access to the resource.
  *                          If empty lets all the logged users access the resource
  * @see utils/roles.js
  * @returns {[function(*=, *=, *): (*|undefined), function(*, *, *): (*|undefined)]}
  */
-const authorizeService = (role= "") => {
+const authorizeService = (role = []) => {
     return [
         // authenticate JWT token and attach user to request object (req.user)
         jwt({ secret, algorithms: ['HS256'], getToken: req => req.cookies.token }),
 
         // authorizeService based on user role
         (req, res, next) => {
-            if (role.length && role !== req.user.role) {
+            if (role.length && !role.includes(req.user.role)) {
                 // user's role is not authorized
                 return res.status(403).json({ message: 'Forbidden' });
             }
