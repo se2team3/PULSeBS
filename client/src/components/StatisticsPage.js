b16f192c3d089004d5e894aaf3151c6dd1ce5d4c
s270325 <s270325@studenti.polito.it>
2020-12-10 20:57:17 +0100

    PULSEBS-145 fixed legend

--- client/src/components/StatisticsPage.js
+++ client/src/components/StatisticsPage.js
@@ -328,7 +328,7 @@ function View(props) {
                                       layout={
                                             {
                                                 barmode: 'stack',
-                                                width: 800,
+                                                width: '100%',
                                                 height: 600,
                                                 title:
                                                 {
@@ -337,7 +337,7 @@ function View(props) {
                                                     x: 0.43,
                                                     xanchor: 'center'
                                                 },
-                                                legend: {font: { size: 16 } },
+                                                legend: {font: { size: 16 },orientation:'h', x:'0.2', y:'-0.17' },
                                                 xaxis: { tickfont: { size: 16 } },
                                                 yaxis: { tickfont: { size: 16 } },
                                             }
