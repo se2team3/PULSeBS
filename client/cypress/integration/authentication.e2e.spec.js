66ba0d5552a666c44b4146f4a45290390ea83900
piazzadario <s276010@studenti.polito.it>
2020-12-16 21:40:05 +0100

    PULSEBS-11 added missing files

--- client/cypress/integration/authentication.e2e.spec.js
+++ client/cypress/integration/authentication.e2e.spec.js
@@ -41,9 +41,9 @@ describe('Header tests', () => {
     });
     it('should properly login with valid credentials', () => {
         cy.get('#username').focus().clear()
-            .type('valid@email.com');
+            .type('email@host.com');
         cy.get('#password').focus().clear()
-            .type('valid_password')
+            .type('passw0rd')
             .type('{enter}');
         cy.getCookie('token');
         // cy.getCookie('token').should('exist');
