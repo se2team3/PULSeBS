35cc5ea977ccaf86c97c1f90c260e111fc26a9da
Luca Rinelli <rinelliluca@gmail.com>
2020-12-17 15:03:59 +0100

    left join for bookings

--- server/daos/teachers_dao.js
+++ server/daos/teachers_dao.js
@@ -16,9 +16,10 @@ exports.getLecturesByTeacherAndTime = function(teacher_id,start_date, end_date) 
         const sql = `SELECT L.id, datetime, course_id, room_id, virtual, L.deleted_at, C.name as course_name,
         T.name as teacher_name, T.surname as teacher_surname,
         R.name as room_name, seats as max_seats, COUNT(B.lecture_id) as booking_counter
- FROM Lectures L, Rooms R, Bookings B, Users T, Courses C
- WHERE L.id=B.lecture_id AND L.course_id=C.id AND L.room_id= R.id AND C.teacher_id = T.id AND C.teacher_id = ? AND L.datetime <= ? AND L.datetime >= ?
- GROUP BY B.lecture_id`
+ FROM Lectures L, Rooms R, Users T, Courses C
+ LEFT JOIN Bookings B ON L.id = B.lecture_id
+ WHERE L.course_id=C.id AND L.room_id= R.id AND C.teacher_id = T.id AND C.teacher_id = ? AND date(L.datetime) <= ? AND date(L.datetime) >= ?
+ GROUP BY L.id`
  
         /* const sql = `SELECT * 
         FROM Lectures L, Users U, Courses C 
