62de335fdf0925df56b082cbb602bcd037eeac63
Federico Bitondo <s276294@studenti.polito.it>
2020-12-28 16:37:11 +0100

    Fixed getLectureById query

--- server/daos/extended_lecture_dao.js
+++ server/daos/extended_lecture_dao.js
@@ -11,19 +11,24 @@ const createExtendedLecture = function (row){
 //gets the lecture with the selected id
 exports.getLectureById = function(id) {
     return new Promise ((resolve,reject) =>{
-        
+
         if(parseInt(id)!==id){
             reject("Is not an integer")
         }
-        
+
         const sql = `
-            SELECT L.id,L.datetime,L.datetime_end,L.course_id,L.room_id,L.virtual, L.deleted_at, C.name as course_name,
-                    T.name as teacher_name, T.surname as teacher_surname, R.name as room_name, R.seats as max_seats, COUNT(B.lecture_id) as booking_counter
-            FROM Lectures L, Users T, Users U, Courses C,Rooms R, Bookings B
-            WHERE L.course_id=C.id AND L.room_id=R.id AND C.teacher_id=T.id AND
-            L.id=B.lecture_id AND B.student_id=U.id AND L.id=? AND T.role="teacher" AND U.role="student"
+            SELECT  L.id, L.datetime, L.datetime_end, L.course_id, L.room_id, L.virtual, L.deleted_at,
+                    C.name as course_name,
+                    T.name as teacher_name, T.surname as teacher_surname,
+                    R.name as room_name, R.seats as max_seats,
+                    COUNT(B.lecture_id) as booking_counter
+            FROM    Lectures L, Users T, Users U, Courses C,Rooms R
+            LEFT JOIN Bookings as B ON B.lecture_id = L.id AND B.student_id=U.id
+            WHERE   L.course_id=C.id AND L.room_id=R.id AND C.teacher_id=T.id AND
+                    L.id = ? AND T.role="teacher" AND U.role="student"
             GROUP BY B.lecture_id
-            ORDER BY L.datetime`;
+            ORDER BY L.datetime 
+        `;
         db.get(sql, [id], (err, row) => {
            if (!row)
                 resolve(null);
