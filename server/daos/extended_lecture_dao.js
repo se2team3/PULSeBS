dc4e3e521a31d2d4010c21a4bb3bf6fd66244906
Federico Bitondo <s276294@studenti.polito.it>
2020-12-28 16:27:15 +0100

    Fixed getLecturesByTeacherId query

--- server/daos/extended_lecture_dao.js
+++ server/daos/extended_lecture_dao.js
@@ -39,17 +39,22 @@ exports.getLectureById = function(id) {
 exports.getLecturesByTeacherId = function(id, from_opt, to_opt) {
     return new Promise ((resolve,reject) =>{
         const sql = `
-            SELECT L.id,L.datetime,L.datetime_end,L.course_id,L.room_id,L.virtual, L.deleted_at, C.name as course_name,
-                T.name as teacher_name, T.surname as teacher_surname, R.name as room_name, R.seats as max_seats, COUNT(B.lecture_id) as booking_counter
-            FROM Lectures L, Users T, Users U, Courses C,Rooms R, Bookings B
-            WHERE L.course_id=C.id AND L.room_id=R.id AND C.teacher_id=T.id AND
-                L.id=B.lecture_id AND B.student_id=U.id AND T.role="teacher" AND U.role="student"
-                AND C.teacher_id = ?
-                AND (? IS NULL OR datetime >= ?)
-                AND (? IS NULL OR datetime <= ?)
-            GROUP BY B.lecture_id
-            ORDER BY L.datetime`;
-        db.all(sql, [id, from_opt, from_opt, to_opt, to_opt], (err, rows) => {
+            SELECT  L.id, L.datetime, L.datetime_end, L.course_id, L.room_id, L.virtual, L.deleted_at,
+                    C.name as course_name,
+                    T.name as teacher_name, T.surname as teacher_surname,
+                    R.name as room_name, R.seats as max_seats,
+                    COUNT(B.lecture_id) as booking_counter
+            FROM    Lectures L, Users T, Users U, Courses C, Rooms R
+            LEFT JOIN Bookings B ON L.id = B.lecture_id
+            WHERE   L.course_id=C.id AND L.room_id=R.id AND C.teacher_id=T.id
+                    AND T.role="teacher" AND U.role="student"
+                    AND C.teacher_id = ?1
+                    AND (?2 IS NULL OR datetime >= ?2)
+                    AND (?3 IS NULL OR datetime <= ?3)
+            GROUP BY L.id
+            ORDER BY L.datetime
+            `;
+        db.all(sql, [id, from_opt, to_opt], (err, rows) => {
             if(err)
                 return reject(err);
             resolve(rows.map(createExtendedLecture));
