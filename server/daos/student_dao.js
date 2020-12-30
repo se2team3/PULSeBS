399a2db71f40fcd32d09377e3708fa6f707ddd8c
Federico Bitondo <s276294@studenti.polito.it>
2020-12-30 17:40:20 +0100

    PULSEBS-253: fixed query bug

--- server/daos/student_dao.js
+++ server/daos/student_dao.js
@@ -16,8 +16,8 @@ exports.retrieveStudentLecturesinTimeFrame = function(student_id,start_date, end
           SELECT L.id, datetime, datetime_end, course_id, room_id, virtual, L.deleted_at, C.name as course_name,
               T.name as teacher_name, T.surname as teacher_surname,
               R.name as room_name, seats as max_seats,
-              sum(case B.waiting when 0 then 1 else 0 end) as booking_counter,
-              sum(case B.waiting when 1 then 1 else 0 end) as waiting_counter,
+              sum(case B.deleted_at IS NULL AND B.waiting = 0 when 1 then 1 else 0 end) as booking_counter,
+              sum(case B.deleted_at IS NULL AND B.waiting = 1 when 1 then 1 else 0 end) as waiting_counter,
               B2.updated_at as booking_updated_at, B2.waiting as booking_waiting, B2.present
           FROM Lectures L, Rooms R, Users T, Courses C
           LEFT JOIN Bookings B2 ON B2.student_id = ?1 AND L.id = B2.lecture_id AND B2.deleted_at IS NULL
